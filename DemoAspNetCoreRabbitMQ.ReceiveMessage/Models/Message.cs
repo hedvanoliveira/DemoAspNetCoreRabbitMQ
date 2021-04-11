b9c5f7b3daf4eeadea18fe8f02dbb42baf55fa24
hedvan <hedvan@gmail.com>
2021-04-11 18:44:52 -0400

    Add ProcessMessage

---
+++ DemoAspNetCoreRabbitMQ.ReceiveMessage/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAspNetCoreRabbitMQ.ReceiveMessage.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ DemoAspNetCoreRabbitMQ.ReceiveMessage/Services/ProcessMessage.cs
@@ -0,0 +1,110 @@
+﻿using DemoAspNetCoreRabbitMQ.ReceiveMessage.Models;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoAspNetCoreRabbitMQ.ReceiveMessage.Services
+{
+    public class ProcessMessage : BackgroundService
+    {
+        private IModel _channel;
+        private IConnection _connection;
+        private readonly string _hostname;
+        private readonly string _queueName;
+        private readonly string _username;
+        private readonly string _password;
+
+        public ProcessMessage()
+        {
+            _hostname = "host.docker.internal";
+            _queueName = "messages";
+            _username = "guest";
+            _password = "guest";
+            InitializeRabbitMqListener();
+        }
+
+        private void InitializeRabbitMqListener()
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = _hostname,
+                UserName = _username,
+                Password = _password
+            };
+
+            _connection = factory.CreateConnection();
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+                var message = JsonConvert.DeserializeObject<Message>(content);
+
+                HandleMessage(message);
+
+                _channel.BasicAck(ea.DeliveryTag, false);
+            };
+            consumer.Shutdown += OnConsumerShutdown;
+            consumer.Registered += OnConsumerRegistered;
+            consumer.Unregistered += OnConsumerUnregistered;
+            consumer.ConsumerCancelled += OnConsumerCancelled;
+
+            _channel.BasicConsume(_queueName, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private void OnConsumerCancelled(object sender, ConsumerEventArgs e)
+        {
+        }
+
+        private void OnConsumerUnregistered(object sender, ConsumerEventArgs e)
+        {
+        }
+
+        private void OnConsumerRegistered(object sender, ConsumerEventArgs e)
+        {
+        }
+
+        private void OnConsumerShutdown(object sender, ShutdownEventArgs e)
+        {
+        }
+
+        private void HandleMessage(Message message)
+        {
+            var json = JsonConvert.SerializeObject(message);
+
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine(json);
+            Console.WriteLine("-----------------------------");
+
+            //using (StreamWriter sr = new StreamWriter(@"BackgroundService.txt"))
+            //{
+            //    var json = JsonSerializer.Serialize(message);
+            //    sr.WriteLine(json);
+            //}
+        }
+    }
+}
